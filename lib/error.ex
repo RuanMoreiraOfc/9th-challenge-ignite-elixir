6af12a88dd9132afab4ad64b908981b71dccd70a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-29 09:37:32 -0300

    feat(Error): add common error handlers

--- lib/error.ex
+++ lib/error.ex
@@ -2,4 +2,25 @@ defmodule Exmeal.Error do
   @keys [:result, :status]
   @enforce_keys @keys
   defstruct @keys
+
+  def wrap(reason) do
+    {:error, reason}
+  end
+
+  def build(status, result) do
+    %__MODULE__{
+      status: status,
+      result: result
+    }
+  end
+
+  def build_not_found(result) do
+    :not_found
+    |> build(result)
+  end
+
+  def build_bad_request(result) do
+    :bad_request
+    |> build(result)
+  end
 end
