eb3216fc63c59d1d817f121b30cd9997dc9035e3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-29 10:21:10 -0300

    feat(MealsController): add `show` treatment

--- lib/exmeal_web/controllers/meals_controller.ex
+++ lib/exmeal_web/controllers/meals_controller.ex
@@ -23,6 +23,11 @@ defmodule ExmealWeb.MealsController do
   end
 
   def show(conn, %{"id" => id}) do
+    with {:ok, %Meal{} = meal} <- Exmeal.get_meal_by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("meal.json", meal: meal)
+    end
   end
 
   def update(conn, params) do
